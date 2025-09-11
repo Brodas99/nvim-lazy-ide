82ea2945fe300ae930004d84527f5eea038700bf
brodas99 <b.rodasdiaz@gmail.com>
2025-09-11 11:59:18 -0700

    good

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -222,4 +222,18 @@ require("lazy").setup({
 		end,
 	},
 
+    
+    -- tailwind-tools.lua
+    {
+      "luckasRanarison/tailwind-tools.nvim",
+      name = "tailwind-tools",
+      build = ":UpdateRemotePlugins",
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim", -- optional
+        "neovim/nvim-lspconfig", -- optional
+      },
+      opts = {} -- your configuration
+    }
+
 })
